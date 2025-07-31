265618723195601b2cc59957eecf1e2ad7e07544
anhdatck02 <92110769+anhdatck02@users.noreply.github.com>
2025-07-31 23:47:24 +0700

    Create data.js

---
+++ Bài_ktra_mẫu_2/data.js
@@ -0,0 +1,62 @@
+const transactions = [
+    {
+        id: 1102,
+        actions: {
+            edit: true,
+            view: true,
+            delete: true
+        },
+        customer: "Võ Hoài An",
+        employee: "Mai Thục Anh",
+        amount: 250000,
+        date: "06 Tháng 6 2024 9:00"
+    },
+    {
+        id: 1199,
+        actions: {
+            edit: true,
+            view: true,
+            delete: true
+        },
+        customer: "Hoàng Thị Thắng",
+        employee: "Nguyễn Văn Hưng",
+        amount: 600000,
+        date: "06 Tháng 6 2024 9:03"
+    },
+    {
+        id: 1239,
+        actions: {
+            edit: true,
+            view: true,
+            delete: true
+        },
+        customer: "Nguyễn Huy Quang",
+        employee: "Nguyễn Văn Hưng",
+        amount: 134000,
+        date: "06 Tháng 6 2024 9:10"
+    },
+    {
+        id: 1677,
+        actions: {
+            edit: true,
+            view: true,
+            delete: true
+        },
+        customer: "Huỳnh Văn Nam",
+        employee: "Mai Thục Anh",
+        amount: 150000,
+        date: "06 Tháng 6 2024 9:25"
+    },
+    {
+        id: 1439,
+        actions: {
+            edit: true,
+            view: true,
+            delete: true
+        },
+        customer: "Nguyễn Hồng Minh",
+        employee: "Mai Thục Anh",
+        amount: 354000,
+        date: "06 Tháng 6 2024 9:24"
+    }
+];
