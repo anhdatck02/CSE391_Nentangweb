4da7fd1e1f9c7af3c5b112f8ec07c4332c49bc24
anhdatck02 <92110769+anhdatck02@users.noreply.github.com>
2025-08-03 23:59:13 +0700

    Create data.js

---
+++ Bai kiem tra mau 1_validate/data.js
@@ -0,0 +1,53 @@
+
+let users = [
+    {
+        id: 1,
+        name: "Nguyễn Văn A",
+        email: "nguyenvana@example.com",
+        address: "175 Tây Sơn, Hà Nội",
+        phone: "0123456788"
+    },
+    {
+        id: 2,
+        name: "Nguyễn Văn B",
+        email: "nguyenvanb@example.com",
+        address: "176 Thanh Xuân, Hà Nội",
+        phone: "0912345678"
+    },
+    {
+        id: 3,
+        name: "Nguyễn Văn C",
+        email: "ngvanc@example.com",
+        address: "177 Nguyễn trãi, Hà Nội",
+        phone: "0982121312"
+    },
+    {
+        id: 4,
+        name: "Phạm Thuỳ Dương",
+        email: "phamthuyduong@example.com",
+        address: "105 Bát Tràng, Hà Nội",
+        phone: "03682232492"
+    },
+    {
+        id: 5,
+        name: "Trần Văn D",
+        email: "tranvand@example.com",
+        address: "202 Bát Khối, Long Biên, Hà Nội",
+        phone: "0965432101"
+    },
+    {
+        id: 6,
+        name: "Đỗ Kim Ngân",
+        email: "dokimngan@example.com",
+        address: "303 Sóc Sơn, Ha Noi",
+        phone: "0934567890"
+    },
+    {
+        id: 7,
+        name: "Nguyễn Phương Hoa",
+        email: "phoa205@example.com",
+        address: "Van Hội,Văn Bính,Thường Tín Hà Nội",
+        phone: "030000000"
+    },
+    
+];
